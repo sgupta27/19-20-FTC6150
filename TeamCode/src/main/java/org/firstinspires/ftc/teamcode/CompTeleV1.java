204fa45c70025bd34ced3e59933bd86ed3103176
Jeremy <Jeremy.yao.info@gmail.com>
2019-01-21 13:20:09 -0500

    undo sahiti's changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTeleV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTeleV1.java
@@ -67,8 +67,8 @@ public class CompTeleV1 extends OpMode
         else if (gamepad2.right_trigger > .2f)
         {
             wristPosition -= .0038;
-            if (wristPosition < 0.0)
-                wristPosition = 0.0;
+            if (wristPosition < .4)
+                wristPosition = .4;
         }
         compRobot.getWristCollectorServo().setPosition(wristPosition);
         telemetry.addData("WristPosition: ", wristPosition);
